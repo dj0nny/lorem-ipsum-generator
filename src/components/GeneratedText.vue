2c01bcc3fb1d089ce2e3cdf7aaf6e9aeb97590b6
dj0nny <frank96990@gmail.com>
2020-07-14 16:53:19 +0200

    add copy message

--- src/components/GeneratedText.vue
+++ src/components/GeneratedText.vue
@@ -2,20 +2,23 @@
   <div class="generated-text-wrapper">
     <textarea id="generatedText" disabled v-model="text"></textarea>
     <button class="button-primary" @click="copyToClipboard">Copy text</button>
+    <span class="copy" v-if="copied">Text copied to clipboard</span>
   </div>
 </template>
 
 <script>
 export default {
   name: 'GeneratedText',
+  data: () => ({
+    copied: false,
+  }),
   props: {
     text: String,
   },
   methods: {
     copyToClipboard() {
       this.$copyText(this.text).then(() => {
-        // eslint-disable-next-line no-alert
-        alert('Text copied');
+        this.copied = true;
       });
     },
   },
